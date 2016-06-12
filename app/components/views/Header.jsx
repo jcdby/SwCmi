73ec102702787a96fac8088765ba385801c785a9
Francois Wavresky <fwavresky@humaxdigital.com>
2016-06-12 18:49:43 +0900

    added forum functions, works well except refresh

--- app/components/views/Header.jsx
+++ app/components/views/Header.jsx
@@ -6,41 +6,44 @@ import NavDropdown from 'react-bootstrap/lib/NavDropdown'
 import MenuItem from 'react-bootstrap/lib/MenuItem'
 import { Link } from 'react-router'
 
-export default class Header extends React.Component {
-  render () {
-    return (
-    <div style={{ marginBottom: 50 }}>
-      <Navbar inverse fixedTop>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <a href="/">수원 선교 교회</a>
-          </Navbar.Brand>
-        </Navbar.Header>
-        <Nav>
-          <NavItem eventKey={1} href="#">
-            교회 소개
-          </NavItem>
-          <NavItem eventKey={2} href="#">
-            설교 동영상
-          </NavItem>
-          <NavItem eventKey={4} href="/gallery">    
-            갤러리 
-          </NavItem>
-          <NavDropdown eventKey={3} title="More" id="basic-nav-dropdown">
-            <MenuItem eventKey={3.1}> Action
-            </MenuItem>
-            <MenuItem eventKey={3.2}> Another action
-            </MenuItem>
-            <MenuItem eventKey={3.3}> Something else here
-            </MenuItem>
-            <MenuItem divider />
-            <MenuItem eventKey={3.3}> Separated link
-            </MenuItem>
-          </NavDropdown>
-        </Nav>
-      </Navbar>
-    </div>
-
-    )
-  }
+export default
+class Header extends React.Component {
+    render() {
+        return (
+            <div style={{marginBottom: 50}}>
+                <Navbar inverse fixedTop>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <a href="/">수원 선교 교회</a>
+                        </Navbar.Brand>
+                    </Navbar.Header>
+                    <Nav>
+                        <NavItem eventKey={1} href="#">
+                            교회 소개
+                        </NavItem>
+                        <NavItem eventKey={2} href="#">
+                            설교 동영상
+                        </NavItem>
+                        <NavItem eventKey={4} href="/gallery">
+                            갤러리
+                        </NavItem>
+                        <NavItem eventKey={5} href="/forum">
+                            Forum
+                        </NavItem>
+                        <NavDropdown eventKey={3} title="More" id="basic-nav-dropdown">
+                            <MenuItem eventKey={3.1}> Action
+                            </MenuItem>
+                            <MenuItem eventKey={3.2}> Another action
+                            </MenuItem>
+                            <MenuItem eventKey={3.3}> Something else here
+                            </MenuItem>
+                            <MenuItem divider />
+                            <MenuItem eventKey={3.3}> Separated link
+                            </MenuItem>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar>
+            </div>
+        )
+    }
 }
--- server/index.js
+++ server/index.js
@@ -1,20 +1,129 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var rootDir = require('./util/get-root-dir')();
 
+
+// --------- all writing about API can be moved to API server,
+// after I understand how to use it. @francois
+var rootDir = require('./util/get-root-dir')();
+var fs = require('fs');
+
+// to parse requests
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+var FORUM_FILE = path.join(__dirname, 'forum.json');
+console.log("FORUM_FILE " + FORUM_FILE);
+
+// -------------- beginning Forum API
+
+
+app.get('/api/forum', function (req, res) {
+    console.log("get /api/forum");
+
+    fs.readFile(FORUM_FILE, function (err, data) {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+        res.json(JSON.parse(data));
+    });
+});
+
+app.get('/api/forum/:id', function (req, res) {
+    var forum_id = req.params.id;
+    console.log("get /api/forum/:id with ID = " + forum_id);
+
+    fs.readFile(FORUM_FILE, function (err, data) {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+        var forum_topic = getById(forum_id, data)[0];
+        console.log('(stringified) forum_topic: \n' + JSON.stringify(forum_topic));
+        res.json(forum_topic);
+    });
+});
+
+app.post('/api/forum/:id', function (req, res) {
+    var forum_id = req.params.id;
+    console.log("post /api/forum/:id with ID = " + forum_id);
+
+    var author = req.body.author;
+    var title = req.body.title;
+    var content = req.body.content;
+
+    // Date.now() for simple ID
+    var new_post = {
+        "post_id": Date.now(),
+        "title": title,
+        "author": author,
+        "content": content
+    };
+
+    console.log("new_post added: " + JSON.stringify(new_post));
+
+    fs.readFile(FORUM_FILE, function (err, data) {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+        var forum_topic = getById(forum_id, data)[0];
+        forum_topic.posts_list.push(new_post);
+
+        var forums_array = add_Post_ForumById(new_post, data, forum_id);
+
+        // rewrite the whole JSON file to update the changes
+        fs.writeFile(FORUM_FILE, JSON.stringify(forums_array), function (err, data) {
+            if (err) {
+                console.error(err);
+                process.exit(1);
+            }
+            // send back just the topic
+            res.json(forum_topic);
+        });
+    });
+
+});
+
+
+function getById(id, data) {
+    var js_array = JSON.parse(data);
+    return js_array.filter(
+        function (data) {
+            return data.id == id
+        }
+    );
+}
+
+function add_Post_ForumById(new_post, data, forum_id) {
+    var forums_array = JSON.parse(data);
+    forums_array.filter(
+        function (data) {
+            if (data.id == forum_id) {
+                data.posts_list.push(new_post);
+            }
+        }
+    );
+    return forums_array;
+}
+
+
+// -------------- end Forum API
 
 app.use('/', express.static(path.join(rootDir, 'public')));
 
 //app.use(express.static('src/public'));
 app.use(express.static('./')); // what is this code for ?? @TODO
 
-app.get('*', function (request, response){
+app.get('*', function (request, response) {
     //response.sendFile(path.resolve(__dirname, 'src/public', 'index.html'))
     response.sendFile(path.resolve(path.join(rootDir, 'public'), 'index.html'))
 });
 
 
 app.listen(3002, function () {
-  console.log('Example app listening on port 3002!');
+    console.log('Example app listening on port 3002!');
 });
