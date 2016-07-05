a1a2a78f479569bc24c27a8e16f181424b30a286
Ethan <jcdby@hotmail.com>
2016-07-05 21:53:42 +0900

    move loginModal compoment to mainlayout so that in every page, you can call out login modal

--- app/components/containers/MainPage.jsx
+++ app/components/containers/MainPage.jsx
@@ -1,41 +1,24 @@
 import React from 'react'
 import HomePage from '../views/HomePage'
 import Header from '../views/Header'
-import LoginModal from '../views//LoginModal'
+
 
 export default class MainPage extends React.Component {
 
   
 
   constructor(props) {
-    super(props);
-    this.state = {
-      showModal: false
-    }
+    super(props);  
 
-    this.clickLogin = this.clickLogin.bind(this);
-    this.closeModal = this.closeModal.bind(this);
   }
 
-  clickLogin() {
-    this.setState({
-      showModal: true
-    })
-  }
-
-  closeModal() {
-    this.setState({
-      showModal: false
-    })
-  }
+  
   
 
   render() {
     return (
       <div>
-        <Header clickLogin={this.clickLogin}></Header>
         <HomePage></HomePage>
-        <LoginModal showModal={this.state.showModal} closeModal={this.closeModal}></LoginModal>
       </div>
     )
   }
--- app/components/layouts/MainLayout.jsx
+++ app/components/layouts/MainLayout.jsx
@@ -2,18 +2,38 @@ import React from 'react'
 //import Header from './../components//Header'
 import Header from '../views/Header'
 import { Footer } from 'rebass'
+import LoginModal from '../views/LoginModal'
 
 export default
-class indexPage extends React.Component {
+    class indexPage extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            showModal: false
+        }
+
+        this.clickLogin = this.clickLogin.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+    }
+
+    clickLogin() {
+        this.setState({
+            showModal: true
+        })
+    }
+
+    closeModal() {
+        this.setState({
+            showModal: false
+        })
     }
 
     render() {
         return (
             <div>
-                <Header></Header>
-              {this.props.children}
+                <Header clickLogin={this.clickLogin}></Header>
+                <LoginModal showModal={this.state.showModal} closeModal={this.closeModal}></LoginModal>
+                {this.props.children}
             </div>
         )
     }
